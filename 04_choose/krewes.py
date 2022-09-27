e78a235331afd051b66a6300f3e283c65728196c
Nicholas Tarsis <ntarsis30@stuy.edu>
2022-09-27 15:20:16 -0400

    added in class work for lab4

---
+++ 04_choose/krewes.py
@@ -0,0 +1,35 @@
+'''
+Flame-Broiled Corgis: Nicholas Tarsis, Anthony Sun, Brian Chen
+Soft Dev
+#4
+22-09-22
+'''
+
+'''
+DISCO:
+* set of dictionary keys is an iterable but cannot be indexed; another list must be generated with the contents of key set
+* random.randrange() takes inclusive/exclusive, random.randint() takes inclusive/inclusive
+* list(I) converts iterable I into a list
+* 
+QCC:
+* What kind of object does krewes.keys() return?
+OPS SUMMARY:
+* Pretty simple procedure to follow
+* Brian searched for functionality online, Nicholas as navigator, Anthony as driver
+* The main function first selects a random period/key and then returns a random devo/value from that period/key
+'''
+
+import random
+
+def get_devo(krewes):
+    period = random.randrange(len(krewes))
+    classlist = krewes[list(krewes.keys())[period]]
+    return classlist[random.randrange(len(classlist))]
+
+krewes = {
+           2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY"], 
+           7:["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "Karen"],
+           8:["ALEKSANDRA",  "NAKIB",  "AMEER",  "HENRY",  "DONALD",  "YAT LONG",  "SEBASTIAN",  "DAVID",  "YUKI",  "SHAFIUL",  "DANIEL",  "SELENA",  "JOSEPH",  "SHINJI",  "RYAN",  "APRIL",  "ERICA",  "JIAN HONG",  "VERIT",  "JOSHUA",  "WILSON",  "AAHAN",  "GORDON",  "JUSTIN",  "MAYA",  "FAIYAZ",  "SHREYA",  "ERIC",  "JEFFERY",  "BRIAN",  "KEVIN",  "SAMSON",  "BRIAN",  "HARRY",  "wanying"]
+         }
+
+print(get_devo(krewes))

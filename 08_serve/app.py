da16216c06afa5c89f206eb852ee33d9c2b428f0
Nicholas Tarsis <ntarsis30@stuy.edu>
2022-10-06 09:21:50 -0400

    started work for app

--- 08_serve/app.py
+++ 08_serve/app.py
@@ -1,15 +1,28 @@
 # your heading here
 
+# Nicholas Tarsis, Jeff Chen, Vansh Saboo
+# Running Hippos
+# SoftDev
+# k08 - serve
+# 10-6-2022
+# time spent:
+# 0.7
+
 from flask import Flask
+import csv
 
 app = Flask(__name__) # Q0: Where have you seen similar syntax in other langs?
 
 @app.route("/") # Q1: What points of reference do you have for meaning of '/'?
-def hello_world():
-    print(__name__) # Q2: Where will this print to? Q3: What will it print?
-    return "No hablo queso!"  # Q4: Will this appear anywhere? How u know?
 
-app.run()  # Q5: Where have you seen similar constructs in other languages?
+
+def hello_world():
+
+    pass
+#    print(__name__) # Q2: Where will this print to? Q3: What will it print?
+#    return "No hablo queso!"  # Q4: Will this appear anywhere? How u know?
+
+#app.run()  # Q5: Where have you seen similar constructs in other languages?
 
 
 '''
@@ -17,7 +30,7 @@ DISCO:
 
 QCC:
 0. Java :O
-1. It looks like the root of a file system. 
+1. It looks like the root of a file system.
 2. This will print to terminal.
 3. It will print __main__, the file from which it was run.
 4. This will appear on the webpage it makes. I know this because I downloaded Flask <3.

d8e5350bb3b2da62b75865085a3f01ca2213bc02
Nicholas Tarsis <ntarsis30@stuy.edu>
2022-10-07 09:16:25 -0400

    got stuff to worto work kind of

--- 08_serve/app.py
+++ 08_serve/app.py
@@ -10,36 +10,41 @@
 
 from flask import Flask
 import csv
-
+import random
+#
 app = Flask(__name__) # Q0: Where have you seen similar syntax in other langs?
-
+#
 @app.route("/") # Q1: What points of reference do you have for meaning of '/'?
 
 
+
+
+def read():
+    jobs = {}
+    with open("./occupations.csv", 'r') as file:
+        csvreader = csv.reader(file)
+        line_count = 0
+        for row in csvreader:
+            if line_count != 0:
+                jobs[row[0]] = float(row[1])
+            line_count = line_count + 1
+        del jobs["Total"]
+    jobs["Other"]= 0.2
+    return jobs
+
+def weighted_select(d):
+    rnum = random.random() * 100
+    running_val_sum = 0
+    line = 0
+    for k in d:
+        if line != 0:
+            running_val_sum += d[k]
+        if running_val_sum >= rnum:
+            return k
+        line += 1
+
+# final test
 def hello_world():
-
-    pass
 #    print(__name__) # Q2: Where will this print to? Q3: What will it print?
-#    return "No hablo queso!"  # Q4: Will this appear anywhere? How u know?
-
-#app.run()  # Q5: Where have you seen similar constructs in other languages?
-
-
-'''
-DISCO:
-
-QCC:
-0. Java :O
-1. It looks like the root of a file system.
-2. This will print to terminal.
-3. It will print __main__, the file from which it was run.
-4. This will appear on the webpage it makes. I know this because I downloaded Flask <3.
-5. Java's driver files.
-...
-
-INVESTIGATIVE APPROACH:
-We downloaded Flask. It gave us a link to the webpage it generated, and on it,
-the returned String was on the webpage. print(__name__) printed "__main__" into
-the terminal. We assume __name__ is the name of the current file it is running
-from. Because app route is "/", we assume main is the default file.
-'''
+    return weighted_select(read())  # Q4: Will this appear anywhere? How u know?
+app.run()  # Q5: Where have you seen similar constructs in other languages?

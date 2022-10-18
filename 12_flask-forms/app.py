6cbfc72285bab4ae9ee9f6fe6cb02ca07523e359
Nicholas Tarsis <ntarsis30@stuy.edu>
2022-10-17 23:14:05 -0400

    current work for lab 12

--- 12_flask-forms/app.py
+++ 12_flask-forms/app.py
@@ -1,74 +1,16 @@
-# Flying Turtles | Anson Wong, Nicholas Tarsis
-# SoftDev
-# K12 -- Echoing Site Visitor Input via an HTML Form
-# 2022-10-17
-# time spent: 1
+from flask import Flask, render_template, request 
 
-from flask import Flask             #facilitate flask webserving
-from flask import render_template   #facilitate jinja templating
-from flask import request           #facilitate form submission
+app = Flask(__name__)   
 
-#the conventional way:
-#from flask import Flask, render_template, request
-
-app = Flask(__name__)    #create Flask object
-
-
-'''
-trioTASK:
-~~~~~~~~~~~ BEFORE RUNNING THIS, ~~~~~~~~~~~~~~~~~~
-...read for understanding all of the code below.
-Some will work as written; other sections will not.
-TASK: Predict which...
-Devise some simple tests you can run to "take apart this engine," as it were.
-Execute your tests.
-Process results.
-PROTIP: Insert your own in-line comments
- wherever they will help
-  your future self and/or current teammates
-   understand what is going on.
-'''
-
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def disp_loginpage():
-    print("\n\n\n")
-    print("***DIAG: this Flask obj ***")
-    print(app)
-    print("***DIAG: request obj ***")
-    print(request)
-    print("***DIAG: request.args ***")
-    print(request.args)
-    #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username']) # won't work since there's no username inputted yet
-    print("***DIAG: request.headers ***")
-    print(request.headers)
-    print("***DIAG: request.method ***")
-    print(request.method)
-    return render_template( 'login.html', request = request.method)
+    return render_template('login.html')
 
-
-@app.route("/auth" , methods=['GET', 'POST'])
+@app.route("/auth", methods=['GET', 'POST']) 
 def authenticate():
-    print("\n\n\n")
-    print("***DIAG: this Flask obj ***")
-    print(app)
-    print("***DIAG: request obj ***")
-    print(request)
-    print("***DIAG: request.args ***")
-    print(request.args)
-    print("***DIAG: request.args['username']  ***")
-    print(request.args['username'])
-    print("***DIAG: request.headers ***")
-    print(request.headers)
-    print("***DIAG: request.form ***")
-    print(request.form)
-    print("***DIAG: request.method ***")
-    print(request.method)
-    return render_template( 'response.html', username = request.args['username'], request = request.method )   #response to a form submission
+    return render_template("response.html", username=request.form['username'], request=request.method)
 
 
-
-if __name__ == "__main__": #false if this file imported as module
-    #enable debugging, auto-restarting of server when this file is modified
-    app.debug = True
+if __name__ == "__main__": 
+    app.debug = True        
     app.run()
